28f96403bfe699b461b346f2ac90f969f7fa3c12
Muhammed-Sadique <132831640+Muhammed-Sadique@users.noreply.github.com>
2023-05-07 20:20:58 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+
+num1 = int(input ("Enter  first number: "))
+num2 = int(input ("Enter second number: "))
+Sum = num1+num2
+sub = num1-num2
+mult= num1*num2
+div = num1/num2
+print(num1, '+', num2, '=', Sum)
+print(num1, '-', num2, '=', sub)
+print(num1, 'X', num2, '=', mult)
+print(num1, '/', num2, '=', div)
+
